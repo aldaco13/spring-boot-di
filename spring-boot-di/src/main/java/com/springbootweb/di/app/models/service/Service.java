60d7388e059e3b82f3c31ffcd69f2c5ab38db2c2
aldaco13 <omar.aldaco.m@gmail.com>
2023-08-23 22:54:05 -0500

    Se agrega inteface

--- spring-boot-di/src/main/java/com/springbootweb/di/app/models/service/Service.java
+++ spring-boot-di/src/main/java/com/springbootweb/di/app/models/service/Service.java
@@ -3,8 +3,9 @@ package com.springbootweb.di.app.models.service;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Service {
+public class Service implements ServiceInterface{
 	
+	@Override
 	public String operacion() {
 		return "Ejecutando proceso...";
 	}
---
+++ spring-boot-di/src/main/java/com/springbootweb/di/app/models/service/ServiceInterface.java
@@ -0,0 +1,7 @@
+package com.springbootweb.di.app.models.service;
+
+public interface ServiceInterface {
+	
+	 public String operacion();
+
+}
